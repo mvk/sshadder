bbce079f5f04069e022bab8d6b0e452331a5bef4
Max Kovgan <maxk@devopsent.biz>
2016-01-11 10:40:26 +0200

    packaging fixes
    - __init__ import fix
    - setup.cfg typo fix
    
    SemVer: bugfix
    
    Signed-off-by: Max Kovgan <maxk@devopsent.biz>

--- sshadder/__init__.py
+++ sshadder/__init__.py
@@ -5,3 +5,5 @@ try:
     __version__ = pkg_resources.get_distribution('sshadder').version
 except pkg_resources.DistributionNotFound:
     __version__ = '0.0.0-unknown'
+    pass
+from sshadder import *
